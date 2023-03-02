b871e9bb20e94c12baed7a02fdf38884130afa63
Diego Carvalho <direisc@gmail.com>
2023-03-02 11:23:29 -0300

    doc: readme

---
+++ src/utils.ts
@@ -0,0 +1,6 @@
+export const cleanUpInterface = (obj: any) => JSON.parse(JSON.stringify(obj))
+
+export const mountResponse = (BODY_MESSAGE: string) =>
+  `<?xml version="1.0" encoding="UTF-8"?>
+<GMF xmlns="com/fiserv/Merchant/gmfV12.04">
+${BODY_MESSAGE}</GMF>`
